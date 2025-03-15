21def7d14e6b55acc0ac522e0fe7c14b44a89c5c
DomanicBlaze040604 <mk7164798@gmail.com>
2025-03-16 01:02:16 +0530

    Delete lib directory

--- lib/features/speech/domain/repositories/speech_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/speech_record.dart';
-
-abstract class SpeechRepository {
-  Future<List<SpeechRecord>> getRecords();
-  Future<void> saveRecord(SpeechRecord record);
-  Future<void> deleteRecord(String id);
-  Future<void> clearRecords();
-  Future<List<String>> getAvailableLanguages();
-}
